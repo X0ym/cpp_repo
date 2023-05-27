15ce5909ff1775ce67b94661b3b59954e2184e3b
yimingxie <yimingxie@58.com>
2023-05-27 15:28:00 +0800

    0527 cpp primer plus

---
+++ src/C++_Primer_Plus/ch2/myfirst.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello C++ Primer Plus";
+    cout << endl; // start a new line
+    cout << "You won't regret it!" << endl;
+    return 0;
+}
+/**
+
+Hello C++ Primer Plus
+You won't regret it!
+
+*/
---
+++ src/C++_Primer_Plus/ch5/forloop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i; // create a counter
+    //   initialize; test ; update
+    for (i = 0; i < 5; i++)
+        cout << "C++ knows loops.\n";
+    cout << "C++ knows when to stop.\n";
+    return 0;
+}
+/*
+
+C++ knows loops.
+C++ knows loops.
+C++ knows loops.
+C++ knows loops.
+C++ knows loops.
+C++ knows when to stop.
+
+*/
