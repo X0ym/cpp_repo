88ddc5a18d005e6231d192ba601b6e4038b103c7
Xieym <59227573+X0ym@users.noreply.github.com>
2023-08-09 22:46:54 +0800

    230809

--- src/C++_Primer_Plus/ch1/two_numbers.cpp
+++ src/C++_Primer_Plus/ch1/two_numbers.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
+/*
+    简单主函数：
+    读取两个数，求它们的和
+*/
 int main()
 {
+    // 提示用户输入两个数
 	cout << "Enter two numbers:" << endl;
-	int v1 = 0 , v2 = 0;
+	int v1 = 0 , v2 = 0;    // 保存读入数据的变量
 
-	cin  >> v1 >> v2 ;
+	cin  >> v1 >> v2 ;      // 读取输入的数据
 	cout << "The mul of " << v1 << " and " << v2
 			  << " is " << v1 * v2 << endl;
 	return 0;
