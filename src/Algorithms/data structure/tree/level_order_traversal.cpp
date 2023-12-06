75c25c9772cb21956e7c30358ad5e8c0879b7618
x0ym <x0ym@github.com>
2023-12-06 22:26:01 +0800

    23.12.06 leval order traversal

--- src/Algorithms/data structure/tree/level_order_traversal.cpp
+++ src/Algorithms/data structure/tree/level_order_traversal.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -9,10 +11,65 @@ struct TreeNode
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+void visit(TreeNode *node) {}
 
+void bfs(TreeNode *root)
+{
+    if (!root)
+        return;
+
+    queue<TreeNode *> q;
+    q.push(root);
+
+    TreeNode *node;
+    while (!q.empty())
+    {
+        // q.size() 此时队列大小即为该层结点数量
+        // 可单独算出该层的结点
+
+        node = q.front();
+        q.pop();
+        visit(node);
+        if (node->left)
+            q.push(node->left);
+        if (node->right)
+            q.push(node->right);
+    }
+}
+
+vector<vector<int>> levelOrder(TreeNode *root)
+{
+    vector<vector<int>> ans;
+
+    if (!root)
+        return ans;
+
+    queue<TreeNode *> q;
+    // 根结点入队
+    q.push(root);
+
+    TreeNode *p = root;
+    while (!q.empty())
+    {
+        int levelSize = q.size();
+        vector<int> l(levelSize);
+        for (int i = 0; i < levelSize; i++)
+        {
+            p = q.front();
+            q.pop();
+            l[i] = p->val;
+            if (p->left)
+                q.push(p->left);
+            if (p->right)
+                q.push(p->right);
+        }
+        ans.push_back(l);
+    }
+    return ans;
+}
 
 int main()
 {
