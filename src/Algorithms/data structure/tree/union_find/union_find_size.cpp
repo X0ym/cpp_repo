9cb42de80348494abd145ba6b83f79aa95ef084a
x0ym <x0ym@github.com>
2023-12-21 09:57:04 +0800

    23.12.21 union find

--- src/Algorithms/data structure/tree/union_find/union_find_size.cpp
+++ src/Algorithms/data structure/tree/union_find/union_find_size.cpp
@@ -1,23 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
 const int N = 100010;
 
-/*
-
-并查集
-
-    初始化：每个元素都属于一个单独的集合，表示为一颗只有根节点的树。
-    初始时，将根节点的父节点设置为自己
-
-
-
-
-*/
-
 // 树的存储方式：只记录父节点 常用于自底向上的递推问题中
 int n;
-int p[N];
+int p[N], size[N];
 
 // 返回 x 的祖宗节点 + 路径压缩
 int find(int x)
@@ -30,14 +19,23 @@ int find(int x)
 // 将一棵树的根节点连到另一棵树的根节点
 void unite(int a, int b)
 {
-    p[find(a)] = find(b);
+    if (find(a) != find(b))
+    {
+        size[find(b)] += find(a);
+        p[find(a)] = find(b);
+    }
 }
 
 int main()
 {
+    int n;
+    cin >> n;
     // 初始化1 每个结点都在单独的一个集合中
     for (int i = 1; i <= n; i++)
+    {
         p[i] = i;
+        size[i] = 1;
+    }
 
     return 0;
 }
