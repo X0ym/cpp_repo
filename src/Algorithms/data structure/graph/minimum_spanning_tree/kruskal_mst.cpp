603981e7e0bdb1973bba663b9b06e348f19cc1fc
x0ym <x0ym@github.com>
2023-12-28 09:12:21 +0800

    23.12.28 mst

--- src/Algorithms/data structure/graph/minimum_spanning_tree/kruskal_mst.cpp
+++ src/Algorithms/data structure/graph/minimum_spanning_tree/kruskal_mst.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -12,6 +13,47 @@ Kruskal 算法是一种常见并且好写的最小生成树算法，由 Kruskal 
 
 
 */
+
+const int N = 100010, M = 200010, inf = 0x3f3f3f3f;
+
+struct edge
+{
+    int u, v, w;
+} edges[M];
+
+int n, m;
+int p[N];
+
+int find(int x)
+{
+    if (p[x] != x)
+        p[x] = find(p[x]);
+    return p[x];
+}
+
+int kruskal()
+{
+    sort(edges, edges + m);
+
+    for (int i = 1; i <= n; i++)
+        p[i] = i;
+
+    int res = 0, cnt = 0;
+    for (int i = 0; i < m; i++)
+    {
+        int u = edges[i].u, v = edges[i].v, w = edges[i].w;
+        u = find(u), v = find(v);
+        if (u != v)
+        {
+            p[u] = v;
+            res += w;
+            cnt++;
+        }
+    }
+    if (cnt < n - 1)
+        return inf;
+    return res;
+}
 
 int main()
 {
--- src/Algorithms/data structure/graph/minimum_spanning_tree/prim_mst.cpp
+++ src/Algorithms/data structure/graph/minimum_spanning_tree/prim_mst.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,12 +10,64 @@ using namespace std;
 Prim 算法是另一种常见并且好写的最小生成树算法。
 该算法的基本思想是从一个结点开始，不断加点（而不是 Kruskal 算法的加边）。
 
-
+朴素 O(n^2)
+二叉堆优化 O()
 
 */
 
+const int N = 510, inf = 0x3f3f3f3f;
+
+int n, m;    // n 个顶点
+int g[N][N]; // 邻接矩阵存图
+int dis[N];  // 每个点到当前最小生成树的距离
+bool vis[N]; // 记录每个顶点是否已经加入最小生成树
+
+// 如果图不连通，返回 inf，否则返回最小生成树的边权之和
+int prim()
+{
+    // 初始化 dis
+    memset(dis, 0x3f, sizeof(dis));
+    dis[1] = 0;
+
+    int res = 0;
+    // 遍历 n 次，需遍历到图中每个顶点
+    for (int i = 0; i < n; i++)
+    {
+        // 寻找未加入最小生成树的点集中距离最小的顶点
+        int t = -1;
+        for (int j = 1; j <= n; j++)
+            if (!vis[j] && (t == -1 || dis[j] < dis[t]))
+                t = j;
+        // 如果顶点不连通说明不存在最小生成树
+        if (dis[t] == inf)
+            return inf;
+        vis[t] = true;
+        res += dis[t];
+
+        // 更新顶点到最小生成树的距离（其实不用再更新已加入的顶点，因为已加入的顶点不影响结果）
+        for (int j = 1; j <= n; j++)
+            dis[j] = min(dis[j], g[t][j]);
+    }
+
+    return res;
+}
+
 int main()
 {
+    cin >> n >> m;
+    memset(g, 0x3f, sizeof g);
+    for (int i = 1; i <= m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u][v] = g[v][u] = min(g[u][v], w); // 处理重边
+    }
+
+    int res = prim();
+    if (res == inf)
+        cout << "impossible";
+    else
+        cout << res;
 
     return 0;
 }
