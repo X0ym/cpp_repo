e7407cd363624ad43e5511c6545a3332a69ef4de
X0ym <xieyiming1997@gmail.com>
2024-01-09 22:39:24 +0800

    24.01.09 string

--- src/Algorithms/string/string_api.cpp
+++ src/Algorithms/string/string_api.cpp
@@ -19,8 +19,32 @@ void append_str()
     cout << name << endl; // bob_alice_
 }
 
+/*
+
+    find(): Find content in string
+        find(str, pos): 在字符串中寻找 str 第一次出现的位置，如果指定 pos 则从
+        pos 开始。
+
+    rfind(): Find last occurrence of content in string
+        rfind(str, pos)：在字符串中寻找 str 最后一次出现的位置，如果指定 pos，
+        则找到的位置，只会小于等于 pos。
+
+*/
+void find_str()
+{
+    string str = "hello worlod!";
+    int pos = str.find("lo");
+    if (pos != str.npos)
+        cout << "find lo pos: " << pos << endl;
+    else
+        cout << "not find lo" << endl;
+}
+
 int main()
 {
-    append_str();
+    // append_str();
+
+    find_str();
+
     return 0;
 }
